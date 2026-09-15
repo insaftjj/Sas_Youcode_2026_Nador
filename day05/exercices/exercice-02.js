286ec0e432fad76261fd726a847099b03c21b899
insaftjj <https://github.com/insaftjj/youcode-sas.git>
2026-09-15 16:24:27 +0200

    j'ai fini les exercice

---
+++ day05/exercices/exercice-02.js
@@ -0,0 +1,23 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * Jour 05 · EXERCICE 02 · NIVEAU 1 : FONDATIONS (DÉBUTANTS)
+ * ACCÈS PAR INDEX
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Déclarez mot = "Programmation". Affichez la première lettre (index 0) et la toute dernière lettre en utilisant mot.length - 1.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-02
+ * ▶️ Commande : node day05/exercices/exercice-02.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+// TODO: écris ta solution ici.
+let mot = "Programmation";
+let premiereLettre = mot[0]; 
+let derniereLettre = mot[mot.length - 1];
+
+console.log("Première lettre :", premiereLettre); 
+console.log(" tout Dernière lettre :", derniereLettre);  
